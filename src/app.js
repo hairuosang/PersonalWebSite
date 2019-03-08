068d566071b05f08e50c00d26831b8710813fd5c
Hairuo Sang <hairuo.sang@gmail.com>
2019-03-08 14:16:49 -0500

    strip down

--- src/app.js
+++ src/app.js
@@ -1,35 +1,13 @@
 import styles from './css/main.scss';
-import Title from './title.js'
-import Prompt from './prompt.js'
 import React from 'react';
-import Presentation from './presentation.js';
 
 export default class App extends React.Component {
-  
+
   displayName: 'App';
   props: any;
   constructor(props){
     super(props);
-    this.state = {position: 0};
-    this.handleScroll = this.handleScroll.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll(event){
-    console.log(event.srcElement.body.scrollBottom);
-
-    if(event.srcElement.body.scrollTop <= 900){
-      this.setState({position: event.srcElement.body.scrollTop});
-    }else{
-      this.setState({position: 900});
-    }
+    this.state = {};
   }
 
   render () {
@@ -41,17 +19,11 @@ export default class App extends React.Component {
       background: "#FFFFFF",
       width: "100%",
       height: "2600px",
-      //height: ((this.props.position < 900) ? "300px": "200px"),
-
-
     }
 
     return (
 
       <div style = {main} >
-        <Title position = {this.state.position} />
-        <Prompt position = {this.state.position} />
-        <Presentation position = {this.state.position} />
       </div>
     );
   }
